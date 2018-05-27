9e069c74b53efba2d5a256f301ed0b8685768f47
Yoichi Sato <nfoxnetwork@gmail.com>
2018-05-27 09:26:55 +0900

    Carthage

---
+++ KeyboardAvoiding/NFKeyboardAvoiding/NFKeyboardAvoiding.h
@@ -0,0 +1,19 @@
+//
+//  NFKeyboardAvoiding.h
+//  NFKeyboardAvoiding
+//
+//  Created by yoichi on 2018/05/27.
+//  Copyright © 2018年 IdleHandsApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NFKeyboardAvoiding.
+FOUNDATION_EXPORT double NFKeyboardAvoidingVersionNumber;
+
+//! Project version string for NFKeyboardAvoiding.
+FOUNDATION_EXPORT const unsigned char NFKeyboardAvoidingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFKeyboardAvoiding/PublicHeader.h>
+
+
